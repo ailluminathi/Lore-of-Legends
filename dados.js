7cd96182ca375c861d19e611dec78a7722545c2c
Náthali Santos <108452211+ailluminathi@users.noreply.github.com>
2024-09-04 17:47:25 -0300

    att

---
+++ dados.js
@@ -0,0 +1,26 @@
+let personagens = [
+    {
+        titulo: "Neeko",
+        descricao: "Nativa de uma tribo vastaya há muito esquecida, Neeko consegue se misturar em qualquer multidão, assumindo a aparência dos outros e até mesmo absorvendo parte de seu estado emocional para identificar de imediato quem é amigo ou inimigo. Nunca se sabe onde Neeko pode estar, nem mesmo quem pode ser, mas aqueles com a intenção de machucá-la logo verão suas verdadeiras cores e toda a força e magia de seu espírito primitivo agindo contra eles.",
+        link: "#",
+        posição : "Meio",
+        região : "Ixtal",
+        raça : "Vastaya"
+    },
+    {
+        titulo: "Teemo",
+        descricao: "Indiferente até aos obstáculos mais perigosos e ameaçadores, Teemo vasculha o mundo com infinito entusiasmo e animação. Um yordle com uma inabalável moral que se orgulha de seguir o Código dos Escoteiros de Bandópolis, às vezes com tanta dedicação que não se toca das possíveis consequências de suas ações. Embora alguns duvidem da existência dos escoteiros, uma coisa é certa: nunca se deve duvidar das convicções de Teemo.",
+        link: "#",
+        posição : "Topo",
+        região : "Bandópolis",
+        raça : "Yordle"
+    },
+    {
+        titulo: "Yuumi",
+        descricao: "Uma gata mágica de Bandópolis, Yuumi já foi a mascote de uma feiticeira yordle chamada Norra. Mas quando sua mestra desapareceu misteriosamente, Yuumi se tornou a Guardiã do Livro dos Portais de Norra, viajando à procura dela através dos portais nas páginas. Sentindo falta do carinho de sua mestra, Yuumi busca diversas companhias em sua jornada, protegendo a todos com escudos luminosos e determinação ferrenha. Enquanto Livro se esforça para mantê-la focada na tarefa, Yuumi costuma se distrair por regalias mundanas, como cochilos e peixe para o jantar. Porém, no fim das contas, ela sempre retoma a missão: encontrar sua querida amiga.",
+        link: "#",
+        posição : "Suporte",
+        região : "Bandópolis",
+        raça : "Gata magica"
+    }
+];
